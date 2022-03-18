9ed7e12c5df949581fd7d50f2fd2af370f325d96
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-03-18 19:14:38 +0800

    add notes: component hierarchy bootstraping root

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
+// A typical Angular app is a hierarchy of modules, with a root module.
+
+// This file is a companion to the index.html page
+
+// import this module from the angular core library
 import { enableProdMode } from '@angular/core';
+// enables to bootstrap app by taking root module as parameter
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +14,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule) // root module
   .catch(err => console.error(err));
