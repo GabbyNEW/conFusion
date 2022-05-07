ff5b085cde8769a9d3cc1bad5e65c02d2b39e411
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-05-08 00:14:21 +0800

    Single Page Application Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -5,10 +5,14 @@ import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 import { MenuComponent } from '../menu/menu.component';
 import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { Component } from "@angular/core";
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
+    // Router parameter, navigate to this view with a parameter called id.
+        // The id parameter will be passed to the dishDetail Component.
+    {path: 'dishdetail/:id', component: DishdetailComponent},
     {path: 'contactus', component: ContactComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'} // default route
 ];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
-
+// Params = Gives you access to the Router parameters
+// ActivatedRoute = Fetch the Route value
+import { Params, ActivatedRoute } from '@angular/router'; 
+import { Location } from '@angular/common'; // Track the location of the page (browser history)
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -11,12 +15,28 @@ export class DishdetailComponent implements OnInit {
   // @Input: A way for you to supply information into a component from another component.
   // You bind a property in the template of the other component,
   // and that will be available as input to this component.
-  @Input() // Automatically refreshes the view when a change in dish value happens.
+  // @Input() // Automatically refreshes the view when a change in dish value happens.
   dish!: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService, 
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    // When you click on a dish from the menuComponent, 
+      // the router link will pass the dish id to the Router as RouterParameter,
+      // and that value will become available here in dishdetailComponent 
+      // by accessing the ActivatedRoute service.
+
+      // You can now ues that value to query the dish here.
+
+    // Fetches the id from the route parameter. Which dish should be shown?
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
