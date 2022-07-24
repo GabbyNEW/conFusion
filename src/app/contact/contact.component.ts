bec97a36b34239f6643239aa83a7c97a381f2baa
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-07-24 21:11:53 +0800

    Animations part two

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit, ViewChild } from '@angular/core'; // ViewChild allows us to access any of the child dom elements in the template
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
+import { flyInOut } from '../animations/app.animation';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  host: { 
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations:[
+    flyInOut()
+  ]
 })
+
 export class ContactComponent implements OnInit {
 
   // Form model that hosts the Reactive form
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,12 +2,22 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish' // so we can use it as a type for a variable
 import { DishService } from '../services/dish.service';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host: { 
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations:[
+    flyInOut(),
+    expand()
+  ]
 })
+
 export class MenuComponent implements OnInit {
   dishes!: Dish[] | undefined;
   errMess: string | undefined;
